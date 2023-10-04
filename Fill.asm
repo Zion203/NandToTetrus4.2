c28f8ef978c3924ecfc036752a1b2ef27e6609ef
Zion203 <141898321+Zion203@users.noreply.github.com>
2023-10-04 16:26:27 +0530

    Add files via upload

---
+++ Fill.asm
@@ -0,0 +1,71 @@
+
+@SCREEN
+D=A
+@ARR
+M=D
+@I
+M=0
+
+@8191
+D=A
+
+@LIMIT
+M=D
+
+
+(CONDITION)
+    @I
+    M=0
+    @24576
+    D=M
+    @LOOP
+    D;JGT
+    
+    (LOOP1)
+        @LIMIT
+        D=M
+        @I
+        D=M-D
+        @CONDITION
+        D;JGE
+        @I
+        D=M
+        @ARR
+        A=M+D
+        M=0
+        @I
+        M=M+1
+        @LOOP1
+        0;JMP
+
+
+    @CONDITION
+    0;JMP   
+    
+    
+
+
+(LOOP)
+    @LIMIT
+    D=M
+    @I
+    
+    D=M-D
+    @END
+    D;JGE
+    @I
+    D=M
+    @ARR
+    A=M+D
+    M=-1
+    @I
+    M=M+1
+
+   
+    @LOOP
+    0;JMP
+
+(END)
+    @CONDITION
+    0;JMP    
+
